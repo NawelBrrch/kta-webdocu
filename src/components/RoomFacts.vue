655de24c44f80b6e9dcf579f8aa28ed44e371080
Klochette <melina.chamayou@gmail.com>
2020-06-10 16:08:04 +0200

    :lipstick: add style

--- src/components/RoomFacts.vue
+++ src/components/RoomFacts.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="roomFacts">
     <p class="roomFacts__text">
-      Les catacombes sont le centre même de l’expression artistique des
-      cataphiles. On y retrouve les oœuvres de très grands street-artists, comme
-      d’amateurs, qui ne cessent d’apprendre. Les grafitis sont mêmes pour
-      certaines de réelles oeuvres d’art.
+      <slot></slot>
     </p>
   </div>
 </template>
@@ -16,15 +13,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-div {
+.roomFacts {
   padding: 47px;
   background-color: hsla(0, 0, 10%, 0.7);
   display: inline-block;
-}
-p {
-  //@include p-style;
-  color: $white;
-  width: 517px;
-  line-height: 170%;
+  &__text {
+    @include p-style;
+    color: $white;
+    width: 517px;
+    line-height: 170%;
+  }
 }
 </style>
