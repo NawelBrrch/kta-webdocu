2593b704033329ebf4f56a1892e7947736ac62e6
cpetillot <constance.petillot@hetic.net>
2020-06-11 18:30:18 +0200

    :building_construction: animating text

--- src/views/Equipment.vue
+++ src/views/Equipment.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="equipment">
     <p class="equipment__text">
-      Pour une meilleure expérience attrapez votre équipement, éteignez les
-      lumières, et branchez votre casque.
+      Pour une meilleure expérience attrapez votre équipement,<br />
+      éteignez les <strong class="text__glow">lumières</strong>, et branchez
+      votre <strong class="text__animation">casque</strong>
     </p>
     <router-link :to="{ name: 'GoingDownVideo' }" tag="button">
       GO DOOOOOWN
@@ -27,7 +28,17 @@ export default { name: "Equipment" };
 }
 
 .equipment__text {
-  width: 50vw;
+  width: 60vw;
   text-align: center;
+  margin-bottom: 5%;
+}
+
+.text__glow {
+  text-shadow: 0 0 20px #efede5, 0 0 15px #efede5, 0 0 20px #f5eabd,
+    0 0 25px #efede5, 0 0 30px #f5eabd, 0 0 35px #f5eabd;
+}
+
+.text__animation {
+  animation: 3s ease-in 1s infinite reverse both running slidein;
 }
 </style>
