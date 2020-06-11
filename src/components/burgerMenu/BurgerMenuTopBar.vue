2ca0d3f2fe61a60bf86472a51d6ed303ccf11f98
Klochette <melina.chamayou@gmail.com>
2020-06-11 14:22:45 +0200

    :dizzy: Add animations for burger menu

--- src/components/burgerMenu/BurgerMenuTopBar.vue
+++ src/components/burgerMenu/BurgerMenuTopBar.vue
@@ -8,32 +8,40 @@
     <transition name="slide">
       <div v-if="isPanelOpen" class="burgerMenu__panel">
         <ul class="burgerMenu__panel--list">
-          <li class="list__item">
+          <li class="list__item" @click="closeTopBarPanel" v-if="isPanelOpen">
             <router-link :to="{ name: 'Homepage' }">
               Accueil
             </router-link>
           </li>
-          <li class="list__item">
+          <li class="list__item" @click="closeTopBarPanel" v-if="isPanelOpen">
             <router-link :to="{ name: 'Gallery' }">Galerie</router-link>
             <ul class="list__item list__item--menu">
-              <li class="list__item">
+              <li
+                class="list__item"
+                @click="closeTopBarPanel"
+                v-if="isPanelOpen"
+              >
                 <router-link :to="{ name: 'Gallery' }">
                   Photos
                 </router-link>
               </li>
-              <li class="list__item">
+              <li
+                class="list__item"
+                @click="closeTopBarPanel"
+                v-if="isPanelOpen"
+              >
                 <router-link :to="{ name: 'Gallery' }">
                   Vidéos
                 </router-link>
               </li>
             </ul>
           </li>
-          <li class="list__item">
+          <li class="list__item" @click="closeTopBarPanel" v-if="isPanelOpen">
             <router-link :to="{ name: 'Sources' }">
               Sources
             </router-link>
           </li>
-          <li class="list__item">
+          <li class="list__item" @click="closeTopBarPanel" v-if="isPanelOpen">
             <router-link :to="{ name: 'About' }">
               À propos
             </router-link>
