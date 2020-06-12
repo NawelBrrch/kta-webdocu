44ce4911ad749163e8875717987c1d845383c353
cpetillot <constance.petillot@hetic.net>
2020-06-12 16:09:45 +0200

    :construction: setting background and CTA to goingDonwHistoryPage

--- src/views/GoingDownHistory.vue
+++ src/views/GoingDownHistory.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>
-    Going Down History
-    <router-link :to="{ name: 'PanoramicView' }" tag="button">
-      Skip
+  <div class="goingDownImage">
+    <router-link class="CTAcontinue" :to="{ name: 'PanoramicView' }">
+      <CTA msg="Continuer" />
     </router-link>
   </div>
 </template>
 
 <script>
-export default {};
+import CTA from "@/components/CTA.vue";
+
+export default {
+  name: "GoingDownHistory",
+  components: {
+    CTA,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.goingDownImage {
+  height: 100vh;
+  width: 100%;
+  background-image: url("../assets/images/descente.png");
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+}
+
+.CTAcontinue {
+  position: relative;
+  z-index: 3;
+  margin-bottom: 5px;
+  margin-left: 20px;
+
+  @include tablet-portrait {
+    margin-left: 35px;
+    margin-bottom: 15px;
+  }
+}
+</style>
