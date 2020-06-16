0d2039389c3fc63a1bd0a30ac36f11a7a29e7e2a
hugo borini <hugo.borini@hetic.net>
2020-06-16 12:27:13 +0200

    :label: {add} text animation

--- src/views/GoingDownHistory.vue
+++ src/views/GoingDownHistory.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="goingDownImage">
+  <div class="goingDownImage__text"><p><span>Les catacombes</span> sont d’anciennes carrières souterraines 
+  de l’époque romaine dont on exploitait la pierre calcaire pour
+   construire la ville de Paris.En 1786, pour pallier à la saturation 
+   des cimetières dûe à la peste noire, la ville ordonne de vider les 
+   cimetières et ses 6millions de cadavres dans les anciennes carrières 
+   abandonnées.Deux siècles plus tard, l’utilisation de ces catacombes est 
+   bien différentes. Dans les années 80 des jeunes redécouvrent l’accès de ces 
+   galeries et commencent à les explorer. Cet espace innocupé devient un lieu de 
+   rencontres, de création artistiques et de fêtes.Ces personnes constituent les premiers
+  groupes de cataphiles. Avec eux se dessine un nouveau monde parallèle</p></div> 
     <router-link class="CTAcontinue" :to="{ name: 'PanoramicView' }">
       <CTA msg="Continuer" />
     </router-link>
@@ -22,7 +32,7 @@ export default {
 //   transform: scale(1.5);
 // }
 .goingDownImage {
-  height: 800px;
+  height: 100vh;
   width: 100%;
   background-image: url("../assets/images/descente.png");
   background-size: cover;
@@ -30,6 +40,25 @@ export default {
   flex-direction: column;
   justify-content: flex-end;
   align-items: flex-start;
+  overflow-Y: hidden;
+  &__text{
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    animation: slideUp 16s;
+    p{
+      font-size: 50px;
+    }
+  }
+
+  @keyframes slideUp{
+    0%{
+      transform: translateY(1000px);
+    }
+    100%{
+      transform: translateY(0px);
+    }
+  }
 }
 
 .CTAcontinue {
